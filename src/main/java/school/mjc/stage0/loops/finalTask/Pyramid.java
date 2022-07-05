3b3724bbb679795c0e80d55cd82ba4d113ac24b2
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-07-05 20:07:10 +0400

    4rd_try

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,17 +3,18 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printHalfPyramid(int cathetusLength) {
         for (int i = 1; i <= cathetusLength; i++) {
-            for (int j = 1; j <= cathetusLength - i; j++) {
+            for (int j = 0; j <= cathetusLength - i; j++) {
                 System.out.print(" ");
             }
             for (int l = i; l >= 1; l--) {
                 System.out.print(l);
             }
+
             for (int r = 2; r <= i; r++) {
                 System.out.print(r);
             }
             for (int j = cathetusLength - 1; j >= i; j--) {
-                System.out.print("");
+                System.out.print(" ");
             }
             System.out.println();
         }
